24bd28654f5d77ddeb4bc3b6c7358a90c2ee59f1
Antonio Salazar <anibal.salazarv@ug.edu.ec>
2024-07-09 16:33:14 -0500

    creación del proyecto conductores en javafx

---
+++ src/demo_jdbc/MainApp.java
@@ -0,0 +1,67 @@
+package demo_jdbc;
+
+import javafx.application.Application;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import demo_jdbc.models.DriverResult;
+import demo_jdbc.respositories.DriverResultRepository;
+
+public class MainApp extends Application {
+
+    private DriverResultRepository driverResultRepository = new DriverResultRepository();
+    private TableView<DriverResult> tableView = new TableView<>();
+    private ObservableList<DriverResult> data = FXCollections.observableArrayList();
+
+    @Override
+    public void start(Stage primaryStage) {
+        ComboBox<Integer> yearComboBox = new ComboBox<>();
+        for (int year = 2000; year <= 2016; year++) {
+            yearComboBox.getItems().add(year);
+        }
+        yearComboBox.setOnAction(event -> updateTableView(yearComboBox.getValue()));
+
+        TableColumn<DriverResult, String> driverNameCol = new TableColumn<>("Driver Name");
+        driverNameCol.setCellValueFactory(new PropertyValueFactory<>("driverName"));
+
+        TableColumn<DriverResult, Integer> winsCol = new TableColumn<>("Wins");
+        winsCol.setCellValueFactory(new PropertyValueFactory<>("wins"));
+
+        TableColumn<DriverResult, Integer> totalPointsCol = new TableColumn<>("Total Points");
+        totalPointsCol.setCellValueFactory(new PropertyValueFactory<>("totalPoints"));
+
+        TableColumn<DriverResult, Integer> rankCol = new TableColumn<>("Rank");
+        rankCol.setCellValueFactory(new PropertyValueFactory<>("rank"));
+
+        tableView.getColumns().add(driverNameCol);
+        tableView.getColumns().add(winsCol);
+        tableView.getColumns().add(totalPointsCol);
+        tableView.getColumns().add(rankCol);
+
+        tableView.setItems(data);
+
+        VBox vbox = new VBox(yearComboBox, tableView);
+        Scene scene = new Scene(vbox, 800, 600);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Driver Results");
+        primaryStage.show();
+    }
+
+    private void updateTableView(int year) {
+        data.clear();
+        data.addAll(driverResultRepository.getResultByYear(year));
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
